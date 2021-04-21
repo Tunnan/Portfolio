e5896fc0dadfcf1fde9965779a91eefa2fd13e95
tunnan <sketsnygg@gmail.com>
2021-04-21 17:20:18 +0200

    Update app.js

--- js/app.js
+++ js/app.js
@@ -6,7 +6,7 @@
 	typeLines(target, [
 		`$ ./hello`,
 		`I'm a web developer based in Sweden with a love for everything open source. Most of my work is done in JavaScript, but I often play around in a lot of other languages.`,
-		`To see more of my projects visit my Github.`,
+		`To see more of my personal projects visit my Github.`,
 		`$ `
 	], 15, () => {
 		target.innerHTML += '<span class="blink">_</span>';
